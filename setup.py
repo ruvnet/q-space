8f0c982d3a3895d9b435e0052a7041ff8ff3e3e6
rUv <ruv@ruv.net>
2024-05-26 05:46:30 +0000

    update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='qspace',
-    version='0.0.2',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=[
         'twine',
